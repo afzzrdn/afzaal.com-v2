25d43b8bda7ffb193d838ed04f6c19b2d4cb7227
afzzrdn <jhendrapeking9@gmail.com>
2025-04-24 15:05:39 +0700

    refactor: add new cv

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 
 
 const Hero = () => {
+  const handleClick = () => {
+    const link = document.createElement('a');
+    link.href = '/CV_Afzaal.pdf'; // Pastikan file berada di public/cv.pdf
+    link.download = 'CV-Muhammad-Afzaal.pdf'; // Nama file saat diunduh
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   return (
     <div className='pl-[40px] md:pl-[350px] xl:pl-[250px] px-[20px] items-start justify-center flex flex-col z-20 h-[550px] w-full md:w-[1500px]'>
       <div className='flex flex-col justify-center items-start gap-3 text-white'>
@@ -11,14 +19,12 @@ const Hero = () => {
         <div className='text-xs sm:text-xl'>
           <p className='text-gray-300 roboto ml-1 mt-2'>These are the jobs I do :</p>
           <div className='flex flex-col md:flex-row mt-2 md:mt-0'>
-            <p className='text-gray-300 roboto ml-1'>Fullstack Developer</p>
+            <p className='text-gray-300 roboto ml-1'>Web Development</p>
             <p className='text-gray-300 roboto ml-1 hidden md:block'>|</p>
-            <p className='text-gray-300 roboto ml-1'>Mobile Developer</p>
-            <p className='text-gray-300 roboto ml-1 hidden md:block'>|</p>
-            <p className='text-gray-300 roboto ml-1'>Software Engineer</p>
+            <p className='text-gray-300 roboto ml-1'>Software Development</p>
           </div>
         </div>
-        <button className='border-[1px] rounded-md hover:scale-105 transition duration-300 ease-in-out border-[#3485ff] py-3 px-10 mt-3 text-sm text-[#3485ff]'>Download CV</button>
+        <button onClick={handleClick} className='border-[1px] rounded-md hover:scale-105 transition duration-300 ease-in-out border-[#3485ff] py-3 px-10 mt-3 text-sm text-[#3485ff]'>Download CV</button>
       </div>
     </div>
   )
